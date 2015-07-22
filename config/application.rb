19f631465b8b1227a67da037c844de688c98520c
RohitRox <link2j.roy@gmail.com>
2015-07-21 22:36:00 -0400

    allow delete in cors request

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module RailsApiJwt
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :delete, :options]
       end
     end
 
